5a78b9c73fd17df2f0f719492b9ae320d3e4f9aa
Dalois-30 <claudetapi@yahoo.com>
2023-05-07 09:09:35 +0100

    nft card basic ok

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Image, Text } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
 
-const NFTCard = () => {
-  return (
-    <View>
-      <Text>NFTCard</Text>
-    </View>
-  )
+import { COLORS, SIZES, SHADOWS, asset } from '../constants';
+
+const NFTCard = ({data}) => {
+    const navigation = useNavigation();
+
+    return (
+        <View style={{
+            backgroundColor: COLORS.white,
+            borderRadius: SIZES.font,
+            marginBottom: SIZES.extraLarge,
+            margin: SIZES.base,
+            ...SHADOWS.dark
+        }} >
+            <View style={{ width: "100%", height: 250 }} >
+                <Image
+                    source={data.image}
+                    resizeMode="cover"
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        borderTopLeftRadius: SIZES.font,
+                        borderTopRightRadius: SIZES.font
+                    }}
+                />
+            </View>
+            <Text>NFTCard</Text>
+        </View>
+    )
 }
 
 export default NFTCard
