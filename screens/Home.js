ae15c3462683e0443a29a2c1db6c3395cd40e931
Dalois-30 <claudetapi@yahoo.com>
2023-05-31 23:39:32 +0100

    app okay

--- screens/Home.js
+++ screens/Home.js
@@ -4,17 +4,31 @@ import { COLORS, NFTData } from "../constants";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
+    const [nftData, setNftData] = useState(NFTData);
+
+    const handleSearch = (value) => {
+        if (!value.length) return setNftData(NFTData);
+
+        const filteredData = NFTData.filter((item) =>
+            item.name.toLowerCase().includes(value.toLowerCase())
+        )
+        if(filteredData.length){
+            setNftData(filteredData)
+        }else {
+            setNftData(NFTData)
+        }
+    }
     return (
         <SafeAreaView style={{ flex: 1 }} >
             <FocusedStatusBar background={COLORS.primary} />
             <View style={{ flex: 1 }} >
                 <View style={{ zIndex: 0 }} >
                     <FlatList
-                        data={NFTData}
-                        renderItem={({ item }) => <NFTCard data={item}/>}
+                        data={nftData}
+                        renderItem={({ item }) => <NFTCard data={item} />}
                         keyExtractor={(item) => item.id}
                         showsVerticalScrollIndicator={false}
-                        ListHeaderComponent={<HomeHeader />}
+                        ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
                     />
                 </View>
                 <View style={{
@@ -25,9 +39,9 @@ const Home = () => {
                     left: 0,
                     zIndex: -1
                 }} >
-                    <View style={{height: 300, backgroundColor: COLORS.primary}} />
-                    <View style={{flex: 1, backgroundColor: COLORS.white}} />
-                    
+                    <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+
                 </View>
             </View>
         </SafeAreaView>
